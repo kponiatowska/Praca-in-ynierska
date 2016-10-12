05cdaa6ab449a7ff136f6c7845e0b747e0425704
AlaBlondi <a.zelewska47@gmail.com>
2016-10-12 19:24:48 +0200

    Stworzylam noego brancha swojgo

--- app/src/main/java/com/inzynierska/opentricity/searcher.java
+++ app/src/main/java/com/inzynierska/opentricity/searcher.java
@@ -3,6 +3,8 @@ package com.inzynierska.opentricity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,25 +14,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+import com.firebase.ui.FirebaseListAdapter;
+import com.firebase.ui.FirebaseRecyclerAdapter;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class searcher extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    Firebase mRootRef;
+    ArrayList<String> mMessages = new ArrayList<>();
+
+    RecyclerView mRecyclerView;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searcher);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +68,16 @@ public class searcher extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        //RECYCLERVIEW
+        Firebase.setAndroidContext(this);
+        mRootRef = new Firebase("https://praca-inzynierska-44519.firebaseio.com");
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -49,6 +87,33 @@ public class searcher extends AppCompatActivity
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+
+
+    protected  void onStart() {
+        super.onStart();
+
+
+        Firebase messageRef = mRootRef.child("restauracje");
+        FirebaseRecyclerAdapter<String, MessageViewHolder> adapter = new FirebaseRecyclerAdapter<String, MessageViewHolder>(String.class, android.R.layout.two_line_list_item, MessageViewHolder.class, messageRef) {
+            @Override
+            protected void populateViewHolder(MessageViewHolder messageViewHolder, String s, int i) {
+                messageViewHolder.mText.setText(s);
+            }
+        };
+
+        mRecyclerView.setAdapter(adapter);
+
+    }
+
+    public static class MessageViewHolder extends  RecyclerView.ViewHolder {
+        TextView mText;
+
+        public MessageViewHolder(View v) {
+            super(v);
+            mText = (TextView) v.findViewById(android.R.id.text1);
         }
     }
 
